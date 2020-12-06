61509da5a8fcbe770c0afa392d7b8e61d9edf8f2
Russell Bjork <bjorkr@DARTIPC1.auth.hpicorp.net>
2020-12-06 07:37:11 -0700

    Added additional code for the data replaced dataset and histogram.

--- working.R
+++ working.R
@@ -3,6 +3,8 @@
 ##
 library(dplyr)
 library(ggplot2)
+library(knitr)
+opts_chunk$set(echo = TRUE)
 
 ##
 ## Read in raw data from activity.csv file
@@ -10,46 +12,88 @@ library(ggplot2)
 rawdata <- read.csv("activity.csv",header=TRUE,na.strings="NA")
 
 ##
-## Transform the data, set NA = 0 from steps and convert date to valid Date
+## Transform the data convert date attribute to valid Date
 ##
 data <- rawdata %>% 
-        # removed as instructed to ignore, this would impact mean calculation
-        # mutate(steps = coalesce(steps, 0)) %>% 
         mutate(date=as.Date(date,format = "%Y-%m-%d"))
 
 ##
-## Data summary and calculations
+## Transform the data NA steps with the average number of steps in the same 5-min interval
+## Create a new dataset from the date transformed, use tapply for filling in the missing values
 ##
-#summary(data)
+datafill <- data
+nas <- is.na(datafill$steps)
+avgintvl <- tapply(datafill$steps, datafill$interval, mean, na.rm=TRUE, simplify=TRUE)
+datafill$steps[nas] <- avgintvl[as.character(datafill$interval[nas])]
+checkdata <- sum(is.na(datafill$steps))
+    print(checkdata)
+
+##
+## Data filtered/grouped summary and calculations
+##
+
+## Data filtered and grouped by date for a summary of steps per day
 dailysteps <- data %>%
     filter(!is.na(steps)) %>%
     group_by(date) %>%
     summarize(steps = sum(steps)) %>%
     print(dailysteps)
 
+## Calculation of mean steps taken each day.
 meansteps <- mean(dailysteps$steps, na.rm=TRUE)
     print(meansteps)
 
+## Calculation of median steps taken each day.
 medsteps <- median(dailysteps$steps, na.rm=TRUE)
     print(medsteps)
     
+## Data filtered and grouped by interval for mean of steps per day
 intvlsteps <- data %>%
     filter(!is.na(steps)) %>%
     group_by(interval) %>%
     summarize(steps = mean(steps))
+    print(intvlsteps)
+
+## Data evaluated to determine interval with maximum steps, on average, across all the days
+maxintvl <- intvlsteps[which.max(intvlsteps$steps),]
+    print(maxintvl)
+        
+## Summarize total missing values in the dataset
+nodata <- sum(is.na(data$steps))
+    print(nodata)
+    
+## Data evaluated to calculate the number of steps taken in each 5-minute interval per day
+stepsfill <- datafill %>%
+    filter(!is.na(steps)) %>%
+    group_by(date) %>%
+    summarize(steps = sum(steps)) %>%
+    print(stepsfill) 
+
+## Calculation of mean steps taken each day on the NA filled data.
+meanstepsfill <- mean(stepsfill$steps, na.rm = TRUE)
+    print(meanstepsfill)
+    
+## Calculation of median steps taken each day on the NA filled data.    
+medianstepsfill <- median(stepsfill$steps, na.rm = TRUE)
+    print(medianstepsfill)
+    
 ##
 ## Graph and plot the transformed data
 ##
-
-# Basic histogram
+# Basic histogram of summarized steps by day
 ggplot(dailysteps, aes(x=steps)) + 
     geom_histogram(binwidth=1000, color="blue", fill="white") +
     labs(title = "Histogram of Steps per day", x = "Steps", y = "Frequency") +
     theme(plot.title.position = 'plot',plot.title = element_text(hjust = 0.5))
 
-# Basic time series
+# Basic time series of mean steps per day
 ggplot(intvlsteps, aes(x=interval, y=steps)) +
     geom_line(color = "blue") +
     labs(title = "Time Series of Steps per Interval", x = "Interval", y = "Steps") +
     theme(plot.title.position = 'plot',plot.title = element_text(hjust = 0.5))
 
+# Basic histogram of summarized steps by day with NA values replaced with interval average data
+ggplot(stepsfill, aes(x = steps)) +
+    geom_histogram(binwidth = 1000, color="blue", fill = "white") +
+    labs(title = "Histogram of Steps per Day - Missing Data Replaced", x = "Steps", y = "Frequency") +
+    theme(plot.title.position = 'plot',plot.title = element_text(hjust = 0.5))
